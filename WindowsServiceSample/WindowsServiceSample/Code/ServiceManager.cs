bf3f63f99d33e980b75c959059171772ae72bb04
byurch <byurchak@gmail.com>
2017-03-17 13:17:22 +0200

    Init

---
+++ WindowsServiceSample/WindowsServiceSample/Code/ServiceManager.cs
@@ -0,0 +1,33 @@
+﻿using SimpleLogger;
+using SimpleLogger.Logging.Handlers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsServiceSample.Code
+{
+	public class ServiceManager
+	{
+		public void DoWork()
+		{
+			Logger.LoggerHandlerManager
+		   .AddHandler(new FileLoggerHandler());
+
+			var fileName = "InfoFile.txt";
+			var fileInfo = new FileInfo(fileName);
+			if (fileInfo.Exists)
+			{
+				Logger.Log(Logger.Level.Fine, File.ReadAllText(fileInfo.FullName));
+			}
+			Logger.Log(Logger.Level.Error, "NO FILE FOUND");
+		}
+
+		//public string IsFilePath(string fullFileName)
+		//{
+		//	Path.GetFileNameWithoutExtension(fullFileName).Equals(fullFileName);
+		//}
+	}
+}
---
+++ WindowsServiceSample/WindowsServiceSample/Service1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WindowsServiceSample.Code;
+
+namespace WindowsServiceSample
+{
+	public partial class Service1 : ServiceBase
+	{
+		public CancellationTokenSource source = new CancellationTokenSource();
+		public Service1()
+		{
+			InitializeComponent();
+		}
+
+		protected override void OnStart(string[] args)
+		{
+			var mngr = new ServiceManager();
+			Task.Run(
+				() => {
+					while (true)
+					{
+						mngr.DoWork();
+						Thread.Sleep(5000);
+					}
+				}, 
+				source.Token);
+		}
+
+		protected override void OnStop()
+		{
+			source.Cancel();
+		}
+	}
+}
